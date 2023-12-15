3ad845720814724fc0836eff7e5a84e4792f8ee4
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2023-12-15 21:38:11 +0530

    login data

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
